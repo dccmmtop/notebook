f5479255250e29e022f2145bfc1d5a4a1fe69361
dccmmtop <dccmmtop@foxmail.com>
2022-07-10 16:04:13 +0800

    update

--- d_blog.rb
+++
@@ -1,7 +0,0 @@
-name = ARGV[0]
-blog_dir = "/mnt/f/blog"
-note_dir = "/mnt/f/notebook"
-cmd =  "cp -r #{note_dir}/images #{blog_dir}/content | cat #{name}  | sed 's/\\.\\.\\/image/\\/image/g' | sed 's/=.*x//g' > #{blog_dir}/content/posts/#{name}"
-puts cmd
-system cmd
- 
